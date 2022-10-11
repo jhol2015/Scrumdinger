fa4960da0133743b6b1f482d493a0f58a7c12180
Jhol <galilleu2011@gmail.com>
2022-10-10 22:55:18 -0300

    add list color and image icon app.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -11,7 +11,7 @@ struct MeetingView: View {
     var body: some View {
         VStack {
             ProgressView(value: 5, total: 15)
-            HStack{
+            HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed").font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
